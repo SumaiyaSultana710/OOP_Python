a0f94c30d39207f9f88e2f860e2f97194accbb4c
Sumaiya <sumaiya.sultana.710@gmail.com>
2022-10-01 12:46:58 +0600

    Class Attributes

--- 2. Constructor/app.py
+++ 2. Constructor/app.py
@@ -1,4 +1,6 @@
 class Item:
+    pay_rate= 0.70
+    all=[]
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -9,11 +11,32 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
 
 print(item1.calculate_total_price())
 print(item2.calculate_total_price())
+
+print(Item.__dict__)
+print(item2.__dict__)
+
+item1.apply_discount()
+item2.apply_discount()
+print(item1.price)
+print(item2.price)
